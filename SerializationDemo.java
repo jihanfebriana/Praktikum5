dce16363322a07990b337940fa0288b5a4b7457a
jihanfebriana <44025122+jihanfebriana@users.noreply.github.com>
2018-10-10 05:21:57 -0700

    Add files via upload

---
+++ SerializationDemo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tugas;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+/**
+ *
+ * @author cynux
+ */
+public class SerializationDemo {
+      public void serialize(String pList, String fileName){
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))){
+            out.writeObject(pList);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public String deserealize(String fileName){
+        String pList = null;
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName))){
+            pList = String.valueOf(in.readObject());
+            
+        } catch (Exception e) {
+                System.out.println(e);
+                System.out.println(e.getMessage());
+        }
+        return pList;
+    }
+    
+    public void saveObject(SerializationDemo obj) throws IOException {
+        try { 
+            FileOutputStream fos = new FileOutputStream("D:/tulisan.txt");
+        ObjectOutputStream out = new ObjectOutputStream(fos);
+        out.writeObject(obj);
+        out.flush();
+        fos.close();
+        }catch (IOException e){
+            System.out.println("Erro di "+e.getMessage());
+        }
+    }
+}
